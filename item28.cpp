ba12683c3ca37b641b3210fd05d6e700aa997001
Anibal Siguenza Torres <atorres@hp-5cd941h0j0.ds1.internal>
2020-05-11 12:04:49 +0200

    add item28

---
+++ item28.cpp
@@ -0,0 +1,47 @@
+/** This items talks about how refernce collapsing works. In short it is an and operation for rvalue reference:
+ * there 2 types of reference so the function is either expecting a lvalue reference or an rvalue reference,
+ * and one can parse either a rvalue or an lvalue wich give us 3 possibilities
+ * lvalue -> f(&) gives a lvalue reference
+ * lvalue -> f(&&) gives a lvalue reference
+ * rvalue -> f(const &) gives a lvalue reference
+ * rvalue -> f(&&) gives a rvalue reference
+ * So only if the reference as well as the value ar r the result will be an r value ref
+ */
+#include <bits/stdc++.h>
+
+template <class T>
+void lReferenceFunction(const T &t) {
+}
+template <class T>
+void rReferenceFunction(T &&t) {
+}
+
+template <typename T>
+class Foo {
+   private:
+   public:
+    typedef T &&RvalueRef;
+};
+
+int main(int argc, char *argv[]) {
+    int lvalue = 0;
+    int &&rvalueref = 0;
+
+    rReferenceFunction(lvalue);  // generates rReferenceFunction<int &>(int &t)
+    rReferenceFunction(123);     // generates rReferenceFunction<int>(int &&t)
+
+    lReferenceFunction(lvalue);  // generates lReferenceFunction<int>(const int &t)
+    lReferenceFunction(123);     // generates lReferenceFunction<int>(const int &t)
+
+    // same for auto
+    auto &&autoLvalue = lvalue;  // int &autoLvalue
+    auto &&autoLvalue = 123;     // int &&autoLvalue
+
+    // same for typedef
+    Foo<int &>::RvalueRef;   // typedef int &Foo<int &>::RvalueRef
+    Foo<int &&>::RvalueRef;  // typedef int &&Foo<int &&>::RvalueRef
+
+    // tricky case: remember that rvaluereference is lvalue so this generates l value call
+    rReferenceFunction(rvalueref);  //rReferenceFunction<int &>(int &t)
+    return 0;
+}
