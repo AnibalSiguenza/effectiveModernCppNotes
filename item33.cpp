e6f61bf6679286f455e1890e8d05a67820ec7934
Anibal Siguenza Torres <atorres@hp-5cd941h0j0.ds1.internal>
2020-05-14 16:17:47 +0200

    add item 33

---
+++ item33.cpp
@@ -0,0 +1,44 @@
+// this items explain how to use perfect forward for lamda function. This available from c++14 on
+#include <bits/stdc++.h>
+
+#include "TestClass.hpp"
+
+void foo(TestClass &tc) {
+    tc.hello();
+    std::cout << "lreference foo" << std::endl;
+}
+void foo(const TestClass &tc) {
+    tc.hello();
+    std::cout << "const lreference foo" << std::endl;
+}
+void foo(TestClass &&tc) {
+    tc.hello();
+    std::cout << "rreference foo" << std::endl;
+}
+
+void foo(TestClass &&tc, int i) {
+    std::cout << "2 argument rvalue foo" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    TestClass tc("lvalue");
+    const TestClass const_tc("const lvalue");
+    auto lamda1 = [](auto &&tc) {  // using auto && is equivalent to use T &&
+        foo(std::forward<decltype(tc)>(tc));
+    };
+
+    lamda1(tc);
+    lamda1(const_tc);
+    lamda1(std::move(TestClass("rvalue1")));
+
+    auto lamda2 = [](auto &&... t) {  // using auto && ... is equivalent to use T && ... args
+        foo(std::forward<decltype(t)>(t)...);
+    };
+
+    lamda2(tc);
+    lamda2(const_tc);
+    lamda2(std::move(TestClass("rvalue2")));
+    lamda2(std::move(TestClass("rvalue3")), 123);
+
+    return 0;
+}
