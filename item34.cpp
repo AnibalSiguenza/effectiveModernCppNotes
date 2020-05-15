5a90ac2775bb599ecec0ef25891ebf73add40ac6
Anibal Siguenza Torres <atorres@hp-5cd941h0j0.ds1.internal>
2020-05-15 14:47:03 +0200

    add item 35.cpp

---
+++ item34.cpp
@@ -0,0 +1,21 @@
+// this item explains how using lamda function is better than using std::bind. Specially after c++14 there is practically no
+// good reason to use bind according to Meyers
+#include <bits/stdc++.h>
+
+using namespace std::placeholders;  // necesary to use the placeholders _1 in blind
+
+void foo(int a, int b) {
+    std::cout << "a+b=" << a + b << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    auto fooL = [](auto a) {
+        foo(a, 10);  // here we set the sencond argument with the desired value and the intension is clear
+    };
+    fooL(2);
+
+    auto fooB = std::bind(foo, _1, 10);
+    fooB(2);
+
+    return 0;
+}
